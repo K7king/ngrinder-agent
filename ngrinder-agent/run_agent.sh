bfa043a88714843cd8759900e5059ad50eeed1e6
K7king <32569102+K7king@users.noreply.github.com>
2017-11-03 11:34:49 +0530

    Update run_agent.sh

--- ngrinder-agent/run_agent.sh
+++ ngrinder-agent/run_agent.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-docker run -d -p 80:80 --name="ngrinder-controller" -h "ngrinder-controller" ngrinder:controller
+ su - root -c docker run -d -p 80:80 --name="ngrinder-controller" -h "ngrinder-controller" ngrinder:controller
 echo ____________DOCKER_CONTROLLER____________
-docker inspect --format '{{ .NetworkSettings.IPAddress }}' ngrinder-controller >>etc/hosts
+su - root -c docker inspect --format '{{ .NetworkSettings.IPAddress }}' ngrinder-controller >>etc/hosts
 echo _______________DOCKER IP SETTING_____________
 curpath=`dirname $0`
 cd ${curpath}
